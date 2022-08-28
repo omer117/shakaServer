93213d644da71da288bd86d5111b66fa17596794
omer117 <omer117@cyber4s.dev>
2022-08-28 22:29:22 +0300

    added products tables to server

--- src/server/postgres.ts
+++ src/server/postgres.ts
@@ -2,7 +2,6 @@ import { Client } from 'pg';
 import dotenv from 'dotenv';
 dotenv.config()
 
-export const PATH ="/Library/PostgreSQL/14/bin/:$PATH"
 export const DATABASE_URL = process.env.DATABASE_URL
 console.log(DATABASE_URL);
 
@@ -14,7 +13,6 @@ export const client = new Client({
 });
 
 client.connect();
-initDb(); //create the tables
 // @ts-ignore
 async function initDb() {
     await client.query(
@@ -24,22 +22,59 @@ async function initDb() {
           price INTEGER NOT NULL,
           info TEXT NOT NULL,
           sizes INTEGER[] NOT NULL,
-          image TEXT NOT NULL,
-      );`
+          image TEXT NOT NULL);`
     );
 
     await client.query(
-        `CREATE TABLE IF NOT EXISTS swimsuit_man(
+        `CREATE TABLE IF NOT EXISTS swimsuit_women(
           id SERIAL PRIMARY KEY,
           title TEXT NOT NULL,
           price INTEGER NOT NULL,
           info TEXT NOT NULL,
           sizes INTEGER[] NOT NULL,
-          image TEXT NOT NULL,
-      );`
+          image TEXT NOT NULL);`
+    );
+    await client.query(
+        `CREATE TABLE IF NOT EXISTS boogi(
+          id SERIAL PRIMARY KEY,
+          title TEXT NOT NULL,
+          price INTEGER NOT NULL,
+          info TEXT NOT NULL,
+          sizes INTEGER[] NOT NULL,
+          image TEXT NOT NULL);`
+    );
+
+    await client.query(
+        `CREATE TABLE IF NOT EXISTS sup(
+          id SERIAL PRIMARY KEY,
+          title TEXT NOT NULL,
+          price INTEGER NOT NULL,
+          info TEXT NOT NULL,
+          sizes INTEGER[] NOT NULL,
+          image TEXT NOT NULL);`
+    );
+
+    await client.query(
+        `CREATE TABLE IF NOT EXISTS soft(
+          id SERIAL PRIMARY KEY,
+          title TEXT NOT NULL,
+          price INTEGER NOT NULL,
+          info TEXT NOT NULL,
+          sizes INTEGER[] NOT NULL,
+          image TEXT NOT NULL);`
+    );
+
+    await client.query(
+        `CREATE TABLE IF NOT EXISTS accessories(
+          id SERIAL PRIMARY KEY,
+          title TEXT NOT NULL,
+          price INTEGER NOT NULL,
+          info TEXT NOT NULL,
+          sizes INTEGER[] NOT NULL,
+          image TEXT NOT NULL);`
     );
     // await client.query(
-    //     `CREATE TABLE IF NOT EXISTS socks(
+    //     `CREATE TABLE IF NOT EXISTS boogi(
     //       sock_id SERIAL PRIMARY KEY,
     //       model TEXT NOT NULL,
     //       quantity INTEGER NOT NULL,
@@ -68,9 +103,9 @@ async function initDb() {
     //     );`
     // );
     
-    // await client.query(
-    //     `INSERT INTO locations (lat,lon,base_name,nearest_city) VALUES (24124678,1410123, 'Ligma', 'Dimona'),(687848,148754, 'Hod-Hashron', 'Kefar Sava'), (987648,3214454, 'Hadera', 'Sdot-Yam');`
-    // );
+    await client.query(
+        `INSERT INTO boogi (title,price,info,sizes,image) VALUES (24124678,1410123, 'Ligma', 'Dimona'),(687848,148754, 'Hod-Hashron', 'Kefar Sava'), (987648,3214454, 'Hadera', 'Sdot-Yam');`
+    );
 
     // await client.query(
     //     `INSERT INTO officers (name,army_identity_number,email,phone_number) VALUES ('MaximusGlorius', 8802308, 'LigmaBalls@gmail.com', 069-4206969);`
@@ -83,4 +118,6 @@ async function initDb() {
     // await client.query(
     //     `INSERT INTO locations_history (arrival_date,departure_date, location_id, sock_id) VALUES ('1945-11-13', '1945-11-12',1, 1);`
     // );         
+    await client.end()
 }
+    initDb(); //create the tables
